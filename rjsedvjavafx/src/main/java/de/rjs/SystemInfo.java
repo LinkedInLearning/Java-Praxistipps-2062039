4aae1678dc96acb55b1265d6a92b259cb5735bd2
Ralph Steyer <ralph.steyer@rjs.de>
2025-09-16 20:57:08 +0200

    Add files via upload

---
+++ rjsedvjavafx/src/main/java/de/rjs/SystemInfo.java
@@ -0,0 +1,13 @@
+package de.rjs;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
